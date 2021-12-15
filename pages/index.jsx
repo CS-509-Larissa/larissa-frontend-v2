a869523b4b5a53a912cc847604a26e7f1d992202
Jack Sullivan <jhaldisul@gmail.com>
2021-12-15 13:12:07 -0500

    svg

--- pages/index.jsx
+++ pages/index.jsx
@@ -3,10 +3,20 @@ import { faHome } from "@fortawesome/free-solid-svg-icons";
 import Loading from "../components/Loading";
 import Tree from "../components/Tree";
 
+import Image from "next/image";
+
+import SvgImage from "/public/undraw_software_engineer_lvl5.svg";
+
 const Home = () => {
   return (
     <>
       <Tree />
+      <div className="svg-container">
+        <img
+          src="/undraw_software_engineer_lvl5.svg"
+          style={{ maxWidth: "500px", marginRight: "100px", flex: "1" }}
+        />
+      </div>
     </>
   );
 };
